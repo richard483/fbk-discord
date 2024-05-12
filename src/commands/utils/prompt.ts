c6010a723a58d8f26ccd28e098d9e6e7a81af907
richard483 <richard.william483@gmail.com>
2024-05-12 22:20:05 +0700

    added axios retry mechanism

--- src/commands/utils/prompt.ts
+++ src/commands/utils/prompt.ts
@@ -1,6 +1,7 @@
 import { SlashCommandBuilder } from 'discord.js';
 import axios from 'axios';
 import { config } from '../../config';
+import axiosRetry from 'axios-retry';
 
 export default {
   data: new SlashCommandBuilder()
@@ -16,6 +17,18 @@ export default {
     await interaction.deferReply();
     const question = interaction.options.getString('question');
     try {
+      axiosRetry(axios, {
+        retries: Number(config.NUMBER_OF_API_RETRY),
+        retryDelay: (count) => {
+          console.log('[RETRY API CALL] Retry attempt: ', count);
+          return count * Number(config.TIME_TO_REPLY);
+        },
+        retryCondition: (error) => {
+          return (
+            error.response?.status === 503 || error.response?.status === 524
+          );
+        },
+      });
       const answer = await axios.post(
         String(config.API_HOST + 'gemini/prompt-text'),
         {
