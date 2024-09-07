c1ede6df338ceb47a41830fa0389ad1bf1102c20
richard483 <richard.william483@gmail.com>
2024-09-07 23:58:45 +0700

    updated nickname

--- src/util/helper.ts
+++ src/util/helper.ts
@@ -2,7 +2,7 @@ export default {
   isCallingBot(text: string) {
     if (!text) return;
     const normalizedText = text.toLowerCase();
-    const panggilan = ['yomi'];
+    const panggilan = ['fubuki', 'buki', 'shirakami', 'fub'];
     return panggilan.some(
       (panggilan) =>
         normalizedText.startsWith(panggilan + ',') ||
