be2f234364e9caf7ef461cf0e17364f6da605152
richard483 <richard.william483@gmail.com>
2024-12-28 15:44:17 +0700

    removed express

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 import { GatewayIntentBits } from 'discord.js';
 import { config } from './config';
 import FbkClient from './util/fbk-client';
-import express from 'express';
 
-const app = express();
 const client = new FbkClient({
   intents: [
     GatewayIntentBits.Guilds,
@@ -14,13 +12,5 @@ const client = new FbkClient({
 
 client.loadCommands();
 client.loadEvents();
-
-app.get('/', (req: any, res: any) => {
-  res.send('Hello from the web server!');
-});
-
-app.listen(config.PORT, () => {
-  console.log('Kon kon kitsune~');
-});
 
 client.login(config.DISCORD_TOKEN).then(() => console.log('Logged in!'));
