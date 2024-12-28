954182f123969a1fd60f421a5b2fe5fd787b8cdd
richard483 <richard.william483@gmail.com>
2024-12-28 15:46:44 +0700

    Revert "removed express"
    This reverts commit be2f234364e9caf7ef461cf0e17364f6da605152.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { GatewayIntentBits } from 'discord.js';
 import { config } from './config';
 import FbkClient from './util/fbk-client';
+import express from 'express';
 
+const app = express();
 const client = new FbkClient({
   intents: [
     GatewayIntentBits.Guilds,
@@ -12,5 +14,13 @@ const client = new FbkClient({
 
 client.loadCommands();
 client.loadEvents();
+
+app.get('/', (req: any, res: any) => {
+  res.send('Hello from the web server!');
+});
+
+app.listen(config.PORT, () => {
+  console.log('Kon kon kitsune~');
+});
 
 client.login(config.DISCORD_TOKEN).then(() => console.log('Logged in!'));
